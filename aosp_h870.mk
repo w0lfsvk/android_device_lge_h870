da40497414c544eb4b36eeb3736ddb1e43c73578
Imaginovskiy <imaginovskiy@kilo-tango.net>
2018-06-01 14:59:31 +0000

    Add bootanimation res

--- aosp_h870.mk
+++ aosp_h870.mk
@@ -8,6 +8,8 @@ $(call inherit-product, vendor/aosp/common.mk)
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := aosp_h870
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
