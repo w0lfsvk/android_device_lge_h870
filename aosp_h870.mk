fd12483e0e3ebd9d000655db17db188f66cca768
Michael <wolfovelp@gmail.com>
2018-06-03 15:36:10 +0200

    Updated aosp_h870.mk

--- aosp_h870.mk
+++ aosp_h870.mk
@@ -8,12 +8,12 @@ $(call inherit-product, vendor/aosp/common.mk)
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1440
 
 PRODUCT_NAME := aosp_h870
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g6" \
+   PRODUCT_DEVICE="g6" \
     PRODUCT_NAME="lucye_global_com" \
-    BUILD_FINGERPRINT="lge/lucye_global_com/lucye:7.0/NRD90U/171732341982c:user/release-keys" \
     PRIVATE_BUILD_DESC="lucye_global_com-user 7.0 NRD90U 171732341982c release-keys"
+    BUILD_FINGERPRINT := "lge/lucye_global_com/lucye:7.0/NRD90U/171732341982c:user/release-keys"
