3322269f3b23e569e90c554b93d569ab9904cfa5
Michael <wolfovelp@gmail.com>
2018-07-11 14:58:21 +0200

    AICPfy

--- aicp.mk
+++ aicp.mk
@@ -3,14 +3,14 @@ $(call inherit-product, device/lge/h870/full_h870.mk)
 # Inherit some common Lineage stuff.
 #$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aicp/config/common.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 TARGET_BOOT_ANIMATION_RES := 1440
 
-PRODUCT_NAME := aosp_h870
+PRODUCT_NAME := aicp_h870
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_DEVICE="g6" \
