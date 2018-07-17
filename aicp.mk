7756db70e5025f0b91055a5146c8dac12107af36
Michael <wolfovelp@gmail.com>
2018-07-17 22:23:03 +0200

    AICPfyv2

--- aicp.mk
+++ aicp.mk
@@ -8,7 +8,9 @@ $(call inherit-product, vendor/aicp/config/common.mk)
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-TARGET_BOOT_ANIMATION_RES := 1440
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
 
 PRODUCT_NAME := aicp_h870
 
